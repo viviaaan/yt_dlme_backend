10e86a0e956c022742ad2a750148a80f51e857a7
Vivian Samuel <viviansamuel42@gmail.com>
2025-07-12 15:52:45 +0530

    add progress reporting

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, request, send_from_directory, send_file
 from flask_cors import cross_origin, CORS
 from yt_dlp import YoutubeDL
@@ -26,11 +27,13 @@ def query(url):
         videoInfo = ydl.sanitize_info(ydl.extract_info(url, download=False))
 
         title = videoInfo['title']
+        id = videoInfo['id']
         thumbnail = videoInfo['thumbnail']
         desc = videoInfo['description']
         duration = videoInfo['duration']
 
         return {'title': title,
+                'id': id,
                 'thumbnail': thumbnail,
                 'description': desc,
                 'duration': duration}
@@ -38,8 +41,22 @@ def query(url):
 @app.route('/download/<path:url>', methods=['POST'])
 @cross_origin(origins=['http://localhost:3000'])
 def download(url):
+    def progressHook(progressInfo):
+        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+        if progressInfo['status'] == 'downloading':
+            percent = ansi_escape.sub('', progressInfo['_percent_str'])
+            eta = ansi_escape.sub('', progressInfo['_eta_str'])
+            if progressInfo['info_dict'].get('id') != None:
+                with open(f"status\\{progressInfo['info_dict']['id']}", 'w') as statusfile:
+                    statusfile.write(json.dumps({'percent': percent, 'eta': eta}))
+        elif progressInfo['status'] == 'finished':
+            if progressInfo['info_dict'].get('id') != None:
+                with open(f"status\\{progressInfo['info_dict']['id']}", 'w') as statusfile:
+                    statusfile.write('finished')
+
     opts = {'continuedl': False,
              'extract_flat': 'discard_in_playlist',
+             'format_sort': ['res:1080', '+size'],
              'fragment_retries': 10,
              'ignoreerrors': 'only_download',
              'no_warnings': True,
@@ -59,9 +76,11 @@ def download(url):
                                  'only_multi_video': True,
                                  'when': 'playlist'}],
              'quiet': False,
+             'progress_hooks': [progressHook],
              'retries': 10,
              'simulate': False,
              'writesubtitles': True,
+             'writeautomaticsub': True,
              'writethumbnail': True}
     with YoutubeDL(opts) as ydl:
         videoInfo = ydl.sanitize_info(ydl.extract_info(url, download=True))
@@ -69,6 +88,16 @@ def download(url):
         print(f'sending {filename}')
 
         return send_file(join(getcwd(), 'data', filename), as_attachment=True)
+
+@app.route('/progress/<id>', methods=['POST'])
+@cross_origin(origins=['http://localhost:3000'])
+def progress(id):
+    with open(f'status\\{id}', 'r') as statusfile:
+        content = statusfile.read()
+    if content != 'finished':
+        return json.loads(content)
+    else:
+        return 'finished'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
